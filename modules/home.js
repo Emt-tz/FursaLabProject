dc1c49d181e94a2a05512484e78c8fd891730c98
Emmanuel Mtera <peterkelvin16@gmail.com>
2022-04-08 11:06:58 +0300

    setup to complete
    soon completion

--- modules/home.js
+++ modules/home.js
@@ -1,7 +1,45 @@
 const path = require("path");
 
-const homeroute = (req, res) =>  {
-res.sendFile(path.join(__dirname, '../', 'index.html'))
+const homeroute = (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'index.html'))
 }
 
-module.exports = {homeroute};
+
+const loginauth = (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    //run sql statement here
+    let validation = true;//validates the data
+    //return response
+    if (validation) {
+        res.redirect("/forums")
+    } else {
+        res.send("invalid credentials");
+    }
+
+}
+
+const registerauth = (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    let confirmpassword = req.body.confirmpassword;
+
+    //check if confirmpassword is equal to password;
+    if (password === confirmpassword) {
+        //run sql statement
+        let validation = true;
+
+        //return reponse
+        if (validation) {
+            res.redirect("/forums");
+        } else {
+            res.send("either user exists or something went wrong");
+        }
+    }else{
+        res.send("passwords input not match")
+    }
+
+}
+
+module.exports = { homeroute, loginauth, registerauth };
